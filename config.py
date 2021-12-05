8c6f82bdabab1ab1c91664c7c5857ce404e53614
TuranHasanov <94192727+TuranHasanov@users.noreply.github.com>
2021-12-05 16:33:43 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = "App id yapışdır"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "api hash yapştır" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "Bot token yapıştır" # 🤖 Get it from @botfather with /newbot command
+    API_ID = "6473933"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "999a64e43ef2d4dc98af4ef69ccd7ade" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "5020652334:AAFptf3gLipGqQMPCcRamEFjuZIbf7AYWo4" # 🤖 Get it from @botfather with /newbot command
